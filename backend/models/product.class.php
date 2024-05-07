08865cd9e63d35d62f77228a4c14bd1c65fe20ed
Niklas Haller <niklash2021@gmail.com>
2024-05-07 14:26:14 +0200

    added admin, user, and product class

---
+++ backend/models/product.class.php
@@ -0,0 +1,23 @@
+<?php
+
+class product
+{
+    public $product_id;
+    public $product_name;
+    public $product_description;
+    public $product_price;
+    public $product_quantity;
+    public $product_category;
+    public $product_imagepath;
+
+    public function __construct($product_id, $product_name, $product_description, $product_price, $product_quantity, $product_category, $product_imagepath)
+    {
+        $this->product_id = $product_id;
+        $this->product_name = $product_name;
+        $this->product_description = $product_description;
+        $this->product_price = $product_price;
+        $this->product_quantity = $product_quantity;
+        $this->product_category = $product_category;
+        $this->product_imagepath = $product_imagepath;
+    }
+}
